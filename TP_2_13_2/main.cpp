d5bfd2bdc4f7648d3b16fc570ee9afd5a7b893a6
danil51 <Danil5124579@gmail.com>
2021-12-18 15:16:33 +0300

    Добавьте файлы проекта.

---
+++ TP_2_13_2/main.cpp
@@ -0,0 +1,57 @@
+#include "helper.h"
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+bool isSplitterWord(const char& ch) {
+	return !isalpha(ch) && !isdigit(ch);
+}
+
+bool isVowel(const char& _ch) {
+	char ch = tolower(_ch);
+	return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y';
+}
+
+std::string task(string& s) {
+	std::string ans = "";
+	bool isWord = false;
+	int startWord = -1;
+	int number = 1;
+	for (int i = 0; i < s.size(); i++) {
+		if (isSplitterWord(s[i])) {
+			if (isWord && isVowel(s[startWord]))
+				s[startWord] = tolower(s[startWord]);
+			ans.append(s.substr(startWord, i - startWord)).append(" ");
+			isWord = false;
+		}
+		else {
+			if (!isWord) {
+				isWord = true;
+				startWord = i;
+			}
+		}
+	}
+	return !ans.empty() ? ans : "В файле пусто\n";
+}
+
+string readFile(ifstream& input) {
+	string s = "";
+	string tmpS;
+	while (getline(input, tmpS))
+		s.append(tmpS).append("\n");
+	return s;
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string filename;
+	processInputNameOfInputFile(filename);
+	ifstream input(filename);
+	string fileBody = readFile(input);
+	cout << "Результат работы:\n" << task(fileBody);
+	input.close();
+}
